3a60b64401cf93bbb73922c047cacad53384c5de
STCN2\n1280406 <yozakura121>
2019-01-31 20:00:41 +0900

    successful morning_bot

--- morning_bot.js
+++ morning_bot.js
@@ -1,1 +1,27 @@
-const = Discord
+require('dotenv').config();
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const token = process.env.TOKEN;
+
+client.on('ready', () => {
+    //準備完了したときの処理をここに書く
+    console.log('準備完了');
+});
+
+client.on('message', message => {
+    if (message.author.bot) {
+        return;
+    }
+    if (message.content === "おはよう") {
+        let channel = message.channel;
+        let author = message.author.channel;
+        let reply_text = "おはよう";
+        message.reply(reply_text)
+            .then(message => console.log(`ok:${reply_text}`))
+            .catch(console.error);
+        return;
+    }
+
+});
+
+client.login(token);
